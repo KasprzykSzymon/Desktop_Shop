9ac0c2cb4e385bcf8bd3af156bfa19076b56bc97
Kasprzyk.Szymon <kasprzyk.szymon.99@gmail.com>
2024-03-27 16:50:15 +0100

    add register screen

--- RTV_SHOP/src/main/java/com/example/rtv_shop/HelloApplication.java
+++ RTV_SHOP/src/main/java/com/example/rtv_shop/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.rtv_shop;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
-        stage.setTitle("Hello!");
+        stage.setTitle("RTV Shop");
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("/Icons/Logo.png")));
+//        stage.getIcons().add(new Image("src/main/resources/icon/Logo.png"));
         stage.setScene(scene);
         stage.show();
     }
